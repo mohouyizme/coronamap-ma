68cc69999112bcd3c4c7f327649b5db4bfc857cb
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-03-17 15:46:25 +0100

    Updates

---
+++ precache-manifest.ac47cac27e359a5c68fa9b80d7bf9089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf39c13d86bd6298fd1712e8022d3eb1",
+    "url": "/coronamap-ma/index.html"
+  },
+  {
+    "revision": "428ed5f14169224b711a",
+    "url": "/coronamap-ma/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d3d2924e03e826a2d41d",
+    "url": "/coronamap-ma/static/js/2.c0ef2fd5.chunk.js"
+  },
+  {
+    "revision": "8c7fe4041954829e8f9a44c5426cd717",
+    "url": "/coronamap-ma/static/js/2.c0ef2fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428ed5f14169224b711a",
+    "url": "/coronamap-ma/static/js/main.000c8cab.chunk.js"
+  },
+  {
+    "revision": "4e10f40f4ac04b63dc16",
+    "url": "/coronamap-ma/static/js/runtime-main.698db6e3.js"
+  }
+]);
